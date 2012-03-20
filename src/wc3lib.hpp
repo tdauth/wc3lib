5809c7b5a2d822af2af30056719bb9a51e83ae96
tamino <barade.barade@web.de>
2012-03-20 20:33:43 +0100

    minor API documentation revision for new website on wc3lib.org

--- src/wc3lib.hpp
+++ src/wc3lib.hpp
@@ -27,11 +27,6 @@
 
 /**
 * \mainpage Warcraft III Library
-* \date Version date
-* 2011-10-20
-* \author Tamino Dauth <tamino@cdauth.eu>
-*
-* \subpage introductionsection Introduction
 * This is the API reference and documentation of the <a url="https://gitorious.org/wc3lib">wc3lib</a> project. wc3lib is an abbreviation which means "Warcraft III Library".
 * It's a collection of varios libraries which allow programmers to use Blizzard's customly developed formats with an abstract C++ interface.
 * All implementations are free and mainly under the GPLv2 license but there's some external code which is mostly under some similar license as well.
@@ -42,8 +37,21 @@
 * Besides you should bear in mind that each module has its own namespace.
 * All namespaces and other declarations of the wc3lib belong to the global namespace \ref wc3lib.
 * Each module uses a header file called "platform.hpp" for its core type definitions such as integer types with specified size.
+* <ul>
+* <li>\subpage coresection </li>
+* <li>\subpage mpqsection </li>
+* <li>\subpage blpsection </li>
+* <li>\subpage mdlxsection </li>
+* <li>\subpage mapsection </li>
+* <li>\subpage editorsection </li>
+* <li>\subpage applicationssection </li>
+* </ul>
 *
-* \subpage coresection Core
+* \date Version date
+* 2011-10-20
+* \author Tamino Dauth <tamino@cdauth.eu>
+*
+* \page coresection Core
 * The core of the wc3lib is shared by all of its modules. It provides some basic functions and classes for exception handling, internationalisation, runtime loading of shared objects/DLLs and binary I/O.
 * Use class \ref LibraryLoader to load and unload shared objects and their symbols at runtime.
 * Class \ref Exception is the base class of all exceptions thrown by functions of the wc3lib.
@@ -51,7 +59,7 @@
 * Class \ref Format is the base class of all format related classes of the wc3lib. It supports some basic serialization member functions.
 * Include \ref utilities.hpp to use many I/O stream functions heavily used by all supported format classes. Besides it includes many default components of the STL and the Boost C++ Libraries and provides some error/info output functions with human-readable messages and class \ref Vertex.
 *
-* \subpage mpqsection MPQ module
+* \page mpqsection MPQ module
 * The MPQ format (Mo'PaQ, short for Mike O'Brien Pack) is Blizzard's archive format used by most of their games.
 * It provides various features:
 * <ul>
@@ -63,7 +71,7 @@
 * Use class \ref wc3lib::mpq::Mpq to access or create one single MPQ archive.
 * Include file \ref mpq.hpp and use namspace \ref wc3lib::mpq to use this module.
 *
-* \subpage blpsection BLP module
+* \page blpsection BLP module
 * This module provides functionality for reading, writing and modifying Blizzard Picture files.
 * Its main class is \ref wc3lib::blp::Blp which represents one single texture.
 * The BLP format supports various features:
@@ -75,19 +83,19 @@
 * </ul>
 * Include file \ref blp.hpp and use namspace \ref wc3lib::blp to use this module.
 *
-* \subpage mdlx MDLX module
+* \page mdlxsection MDLX module
 * This module provides several classes to read and write Blizzard's 3d graphics formats MDX (binary) and MDL (human-readable).
 * It does not provide any display/rendering functionality. Use module \ref editor for this.
 * Class \ref wc3lib::mdlx::Mdlx provides all functionality which is needed to save or load one MDX or MDL file.
 * It can easily be used to convert one format into the other one (application "wc3converter" provides this feature).
 * Include file \ref mdlx.hpp and use namespace \ref wc3lib::mdlx to use this module.
 *
-* \subpage mapsection Map module
+* \page mapsection Map module
 * Use this module to read, write and modify Warcraft III maps (*.w3m, *.w3x) and campaigns (*.w3n) and their corresponding files.
 * Warcraft III maps and campaigns are usual MPQ archives containing some specific files, most of them in a binary format.
 * Include file \ref map.hpp and use namespace \ref wc3lib::map to use this module.
 *
-* \subpage editorsection Editor module
+* \page editorsection Editor module
 * The editor module contains many classes to emulate the original World Editor created by Blizzard Entertainment.
 * It's based on some plugins which can be used without the whole editor module functionality, as well.
 * Include file \ref editor.hpp and use namspace \ref wc3lib::edtor to use this module.
@@ -96,7 +104,7 @@
 * \section pluginssection Plugins
 * There is various plug-ins which allow you to load and save BLP textures in Qt/KDE GUIs and OGRE scenes and to access MPQ archives via KDE's KIO slave module.
 *
-* \subpage applicationssection Applications module
+* \page applicationssection Applications module
 * Since this module only provides some useful applications there is neither any namespace to use nor any header file to include.
 * Currently there are following applications available:
 * <ul>
