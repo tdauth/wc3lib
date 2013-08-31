0dd62228e5c5621b8f54725b94ceee15727789fa
barade <barade.barade@web.de>
2013-08-31 23:02:21 +0200

    added missing files

---
+++ src/map/triggerstrings.cpp
@@ -0,0 +1,136 @@
+/***************************************************************************
+ *   Copyright (C) 2013 by Tamino Dauth                                    *
+ *   tamino@cdauth.eu                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/foreach.hpp>
+
+#include "triggerstrings.hpp"
+
+namespace wc3lib
+{
+
+namespace map
+{
+
+TriggerStrings::TriggerStrings()
+{
+}
+
+TriggerStrings::Entry::Entry()
+{
+}
+
+void TriggerStrings::readFunction(const Txt::Pair &ref, Entries &functions)
+{
+	string code = ref.first;
+	bool isHint = false;
+	bool isFirst = false;
+	string::size_type index = code.find_last_of("Hint");
+	
+	if (index + strlen("Hint") == code.size()) {
+		isHint = true;
+		code = code.substr(0, index);
+	}
+	
+	Entries::iterator iterator = functions.find(code);
+	
+	if (iterator == functions.end()) {
+		iterator = functions.insert(code, new Entry()).first;
+		std::cerr << "New entry \"" << code << "\"" << std::endl;
+		iterator->second->setCode(code);
+		iterator->second->setName(ref.second);
+	} else if (isHint) {
+		iterator->second->setHint(ref.second);
+	} else {
+		iterator->second->setLayout(ref.second);
+	}
+}
+
+std::streamsize TriggerStrings::read(TriggerStrings::InputStream& istream) throw (class Exception)
+{
+	boost::scoped_ptr<Txt> txt(new Txt());
+	std::streamsize size = txt->read(istream);
+	
+	BOOST_FOREACH(Txt::Pairs::const_reference ref, txt->entries("TriggerEventStrings")) {
+		readFunction(ref, this->events());
+	}
+	
+	BOOST_FOREACH(Txt::Pairs::const_reference ref, txt->entries("TriggerConditionStrings")) {
+		readFunction(ref, this->conditions());
+	}
+	
+	BOOST_FOREACH(Txt::Pairs::const_reference ref, txt->entries("TriggerActionStrings")) {
+		readFunction(ref, this->actions());
+	}
+	
+	BOOST_FOREACH(Txt::Pairs::const_reference ref, txt->entries("TriggerCallStrings")) {
+		readFunction(ref, this->calls());
+	}
+	
+	return size;
+}
+
+std::streamsize TriggerStrings::write(TriggerStrings::OutputStream& ostream) const throw (class Exception)
+{
+    return 0;
+}
+
+TriggerStrings::Entries& TriggerStrings::entries(BOOST_SCOPED_ENUM(TriggerFunction::Type) type)
+{
+	switch (type) {
+		case TriggerFunction::Type::Event:
+			return this->events();
+		
+		case TriggerFunction::Type::Condition:
+			return this->conditions();
+			
+		case TriggerFunction::Type::Action:
+			return this->actions();
+			
+		case TriggerFunction::Type::Call:
+			return this->calls();
+	}
+	
+	throw 0;
+}
+
+const TriggerStrings::Entries& TriggerStrings::entries(BOOST_SCOPED_ENUM(TriggerFunction::Type) type) const
+{
+	switch (type) {
+		case TriggerFunction::Type::Event:
+			return this->events();
+		
+		case TriggerFunction::Type::Condition:
+			return this->conditions();
+			
+		case TriggerFunction::Type::Action:
+			return this->actions();
+			
+		case TriggerFunction::Type::Call:
+			return this->calls();
+	}
+	
+	throw 0;
+}
+
+
+}
+
+}
