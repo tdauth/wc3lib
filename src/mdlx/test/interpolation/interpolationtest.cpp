318a390b831d4070f90a84d2efb26c08f9efea21
barade <barade.barade@web.de>
2014-07-27 04:54:38 +0200

    Initial Interpolator class
    - added initial interpolator class to the mdlx module
    - added missing test files for the mdlx module

---
+++ src/mdlx/test/interpolation/interpolationtest.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2014 by Tamino Dauth                                    *
+ *   tamino@cdauth.eu                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#define BOOST_TEST_MODULE InterpolationTest
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include <iostream>
+
+//#include <boost/foreach.hpp>
+
+//#include "../../platform.hpp"
+#include "../../platform.hpp"
+#include "../../mdlgrammar.hpp"
+#include "../../interpolator.hpp"
+
+#ifndef BOOST_TEST_DYN_LINK
+#error Define BOOST_TEST_DYN_LINK for proper definition of main function.
+#endif
+
+using namespace wc3lib;
+using namespace wc3lib::mdlx;
+
+BOOST_AUTO_TEST_CASE(GeosetAnim) {
+	spiritTraceLog.close();
+	spiritTraceLog.open("geosetanim_traces.xml");
+
+	BOOST_REQUIRE(spiritTraceLog);
+
+	ifstream in("geosetanim.mdl");
+
+	BOOST_REQUIRE(in);
+
+	Mdlx *result = 0;
+	MdlGrammar grammar;
+	BOOST_REQUIRE(grammar.parse(in, result));
+	BOOST_REQUIRE(result);
+}
