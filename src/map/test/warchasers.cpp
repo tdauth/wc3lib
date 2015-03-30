95f0bf3c45893900102718dd9a37f37ac43397f0
barade <barade.barade@web.de>
2015-03-30 22:04:46 +0200

    Fixed and increased War Chasers test

--- src/map/test/warchasers.cpp
+++ src/map/test/warchasers.cpp
@@ -145,13 +145,133 @@ BOOST_AUTO_TEST_CASE(Cameras)
 	BOOST_REQUIRE(map.cameras()->cameras().size() == 6);
 	const map::Camera &cam0 = map.cameras()->cameras()[0];
 	BOOST_REQUIRE(cam0.name() == "CamStart1");
-	BOOST_CHECK_CLOSE(cam0.targetX(), -7718.41, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.targetY(), -9039.14, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.zOffset(), 0.00, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.rotation(), 90.0, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.angleOfAttack(), 304.00, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.distance(), 1790.91, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.roll(), 0.00, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.fieldOfView(), 70.00, 0.0001);
-	BOOST_CHECK_CLOSE(cam0.farZ(), 5000.00, 0.0001);
+	BOOST_CHECK_CLOSE(cam0.targetX(), -7718.41, 0.001);
+	BOOST_CHECK_CLOSE(cam0.targetY(), -9039.14, 0.001);
+	BOOST_CHECK_CLOSE(cam0.zOffset(), 0.00, 0.001);
+	BOOST_CHECK_CLOSE(cam0.rotation(), 90.0, 0.001);
+	BOOST_CHECK_CLOSE(cam0.angleOfAttack(), 304.00, 0.001);
+	BOOST_CHECK_CLOSE(cam0.distance(), 1790.91, 0.001);
+	BOOST_CHECK_CLOSE(cam0.roll(), 0.00, 0.001);
+	BOOST_CHECK_CLOSE(cam0.fieldOfView(), 70.00, 0.001);
+	BOOST_CHECK_CLOSE(cam0.farZ(), 5000.00, 0.001);
+
+	const map::Camera &cam1 = map.cameras()->cameras()[1];
+	BOOST_REQUIRE(cam1.name() == "Camera 002");
+	BOOST_CHECK_CLOSE(cam1.targetX(), 2595.08, 0.001);
+	BOOST_CHECK_CLOSE(cam1.targetY(), -8045.40, 0.001);
+	BOOST_CHECK_CLOSE(cam1.zOffset(), 0.00, 0.001);
+	BOOST_CHECK_CLOSE(cam1.rotation(), 133.47, 0.001);
+	BOOST_CHECK_CLOSE(cam1.angleOfAttack(), 314.55, 0.001);
+	BOOST_CHECK_CLOSE(cam1.distance(), 1996.50, 0.001);
+	BOOST_CHECK_CLOSE(cam1.roll(), 0.00, 0.001);
+	BOOST_CHECK_CLOSE(cam1.fieldOfView(), 70.00, 0.001);
+	BOOST_CHECK_CLOSE(cam1.farZ(), 5000.00, 0.001);
 }
+
+BOOST_AUTO_TEST_CASE(Rects)
+{
+	map::W3m map;
+
+	bool valid = true;
+
+	try
+	{
+		map.open("(4)WarChasers.w3m");
+	}
+	catch (std::exception &e)
+	{
+		valid = false;
+
+		std::cerr << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		valid = false;
+	}
+
+	BOOST_REQUIRE(valid);
+
+	/*
+	 * Rects
+	 */
+	bool validRects = true;
+
+	try
+	{
+		map.readFileFormat(map.rects().get());
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		validRects = false;
+	}
+	catch (...)
+	{
+		validRects = false;
+	}
+
+	BOOST_REQUIRE(validRects);
+	// Object Manager lists this number
+	BOOST_REQUIRE(map.rects()->rects().size() == 151);
+	const map::Rect &rect0 = map.rects()->rects()[0];
+	BOOST_REQUIRE(rect0.name() == "ALL SPIDERS");
+	BOOST_CHECK_CLOSE(rect0.left(), -8000, 0.001);
+	BOOST_CHECK_CLOSE(rect0.right(), -6400, 0.001);
+	BOOST_CHECK_CLOSE(rect0.bottom(), 2048, 0.001);
+	BOOST_CHECK_CLOSE(rect0.top(), 4512, 0.001);
+	BOOST_CHECK(rect0.color() == 0xFFFFFF);
+	BOOST_CHECK(!rect0.hasWeatherEffect());
+	BOOST_CHECK(!rect0.hasSound());
+}
+
+BOOST_AUTO_TEST_CASE(CustomUnits)
+{
+	map::W3m map;
+
+	bool valid = true;
+
+	try
+	{
+		map.open("(4)WarChasers.w3m");
+	}
+	catch (std::exception &e)
+	{
+		valid = false;
+
+		std::cerr << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		valid = false;
+	}
+
+	BOOST_REQUIRE(valid);
+
+	/*
+	 * Custom Units
+	 */
+	bool validCustomUnits = true;
+
+	try
+	{
+		map.readFileFormat(map.customUnits().get());
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		validCustomUnits = false;
+	}
+	catch (...)
+	{
+		validCustomUnits = false;
+	}
+
+	BOOST_REQUIRE(validCustomUnits);
+	// Object Manager lists this number
+	BOOST_REQUIRE(map.customUnits()->originalTable().size() == 122);
+	BOOST_REQUIRE(map.customUnits()->customTable().size() == 25);
+	const map::CustomUnits::Unit &unit0 = map.customUnits()->originalTable()[0];
+	BOOST_CHECK(map::idToString(unit0.originalId()) == "hmtt");
+	BOOST_CHECK(map::idToString(unit0.customId()) == "");
+}
+
