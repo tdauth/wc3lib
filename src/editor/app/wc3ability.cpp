e3a69dfa0dca84e40d48ce666b15927ae3040286
barade <barade.barade@web.de>
2016-01-30 08:06:53 +0100

    Added ability editor

---
+++ src/editor/app/wc3ability.cpp
@@ -0,0 +1,90 @@
+/***************************************************************************
+ *   Copyright (C) 2014 by Tamino Dauth                                    *
+ *   tamino@cdauth.eu                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QScopedPointer>
+#include <QMainWindow>
+
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KLocale>
+#include <KMessageBox>
+
+#include "../../editor.hpp"
+#include "../../exception.hpp"
+
+#include "../objecteditor/abilityeditor.hpp"
+#include "../objecteditor/objecttreemodel.hpp"
+
+using namespace wc3lib::editor;
+
+int main(int argc, char *argv[])
+{
+	KAboutData aboutData(Editor::aboutData());
+
+	KCmdLineArgs::init(argc, argv, &aboutData);
+	KCmdLineOptions options;
+	options.add("", ki18n("Additional help."));
+	options.add("+[file]", ki18n("File to open"));
+	KCmdLineArgs::addCmdLineOptions(options);
+
+	KApplication app;
+
+	QScopedPointer<MpqPriorityList> source(new MpqPriorityList());
+	source->sharedData()->sharedObjectData()->unitEditorData()->setSource(source.data());
+
+	try
+	{
+		source->sharedData()->sharedObjectData()->unitEditorData()->load();
+	}
+	catch (wc3lib::Exception &e)
+	{
+		KMessageBox::error(0, e.what());
+	}
+
+	QScopedPointer<AbilityData> abilityData(new AbilityData(source.data()));
+	AbilityEditor editor(source.data(), abilityData.data(), 0);
+
+	try
+	{
+		abilityData->load(&editor);
+		editor.treeModel()->load(source.data(), abilityData.data(), &editor);
+	}
+	catch (wc3lib::Exception &e)
+	{
+		KMessageBox::error(0, e.what());
+	}
+
+	QMainWindow mainWindow;
+	mainWindow.setCentralWidget(&editor);
+	mainWindow.show();
+
+	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+	if (args != 0)
+	{
+		for (int i = 0; i < args->count(); ++i)
+		{
+			// TODO import all objects into Unit Editor
+		}
+	}
+
+	return app.exec();
+}
