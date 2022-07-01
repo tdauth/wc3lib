c62281be0969a2bef7fdf522bd65dfafe14ca1f3
Tamino Dauth <taminojdauth@gmail.com>
2022-07-01 18:54:52 +0200

    Improve Ubuntu install script for the latest Ubuntu versions

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Install all dependencies and build the binaries.
 # qtcreator is useful for building and developing wc3lib.
-sudo apt install cmake-gui gettext libboost-all-dev libjpeg-dev libbz2-dev qt5-default qtmultimedia5-dev qtcreator
+sudo apt install cmake-gui gettext libboost-all-dev libjpeg-dev libbz2-dev qt5-default qtmultimedia5-dev qtcreator libogre-1.9-dev libkf5kdelibs4support-dev doxygen
 SOURCE_DIR=$(pwd)
 mkdir ../wc3lib-build
 cd ../wc3lib-build
-cmake "$SOURCE_DIR"
+cmake "$SOURCE_DIR" -DCMAKE_MODULE_PATH="/usr/share/OGRE/cmake/modules/" -DBLP=ON -DMPQ=ON -DMDLX=ON -DMAP=ON -DJASS=ON -DAPP=ON -DEDITOR=ON -DUSE_KIOSLAVE=ON -DUSE_QBLP=ON -DUSE_OGREBLP=ON -DGAME=ON -DUSE_MIME=ON -DDOC=ON
 make -j4
