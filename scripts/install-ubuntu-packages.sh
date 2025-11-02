e9496e0dfef245c19f33acce1d93fca61b3a2357
Tamino Dauth <taminojdauth@gmail.com>
2025-11-02 01:41:02 +0100

    dependencies

--- scripts/install-ubuntu-packages.sh
+++ scripts/install-ubuntu-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install all dependencies and build the binaries.
-sudo apt install cmake-gui gettext libboost-all-dev libjpeg-dev libbz2-dev qtbase5-dev qtmultimedia5-dev libogre-1.12-dev extra-libkf5kdelibs4support-dev cmake-modules doxygen
+sudo apt install cmake-gui gettext libboost-all-dev libjpeg-dev libbz2-dev qtbase5-dev qtmultimedia5-dev libogre-1.12-dev libogre-1.12-dev extra-cmake-modules libkf5kio-dev doxygen
